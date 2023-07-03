6ff1790e3dbc1243c9b2b83890e5e7abeb00366f
piotrowicz0105 <apiotrowicz@outlook.fr>
2023-07-03 16:28:42 +0200

    Config reactjs and tailwindcss ready

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import SearchBar from '../SearchBar/SearchBar';
+import '../../index.css';
+
+function App() {
+  return (
+    <div className="App font-overpass">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar(props) {
+  return (
+
+    <div className=" bg-slate-600">test</div>
+
+  );
+}
+
+export default SearchBar;
